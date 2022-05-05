7cf87b595752f50438ca4b587e60c64a1ac0b0ec
Jean-Marc <jm.kleger@gmail.com>
2022-05-05 23:19:48 +0200

    Test connection to DB with port 3000

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function HomePage({ data }) {
 			<h1>Current & next games</h1>
 			{/* <div style={{ height: '400px', border: '1px dotted grey' }}>Box</div> */}
 			{/* {Date()}<br /> */}
-			Last deployment: Thursday, 23:11.
+			Last deployment: Thursday, 23:19.
 			<div className={styles.container}>
 				{data && data.map((event: Event) =>
 					<Link key={event.id} href={`/events/${event.id}`}>
