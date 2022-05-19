21d659c75c82936ab2b78ce36e35ecc0a722dc35
unknown <jm.kleger@gmail.com>
2022-05-19 14:39:14 +0200

    Updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <meta name="robots" content="noindex,nofollow" />
-            <Head />
+            <Head>
+                <meta name="robots" content="noindex,nofollow" />
+                <link rel="shortcut icon" href="/favicon.png" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
