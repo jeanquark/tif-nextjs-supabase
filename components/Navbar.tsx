2741e9e7595a9a046e620188fcf4d0affc86ce89
Jean-Marc <jm.kleger@gmail.com>
2022-05-20 00:26:30 +0200

    Test change of Supabase URL variable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -252,7 +252,7 @@ export default function Navbar() {
                                 <option value="fr">FR Français</option>
                             </select>
                         </li>
-                        {auth &&
+                        {auth && auth.points &&
                             <>
                                 <li className={styles.navigationLi}><b>{auth.points}&nbsp;points</b></li>
                                 <li className={styles.navigationLi}>
